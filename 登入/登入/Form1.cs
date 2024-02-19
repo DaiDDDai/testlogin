7c67fe2f39526dd23bda4715c8022c3d50445934
DaiDDDai <158536211+DaiDDDai@users.noreply.github.com>
2024-02-20 00:48:08 +0800

    Add files via upload

---
+++ 登入/登入/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace 登入
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            string connectionString = "Data Source=DESKTOP-RDBCCLG\\SQLEXPRESS;Initial Catalog = membersystem;Integrated Security=True";
+
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("錯誤：欄位空白，請填入帳號密碼");
+                return; // 在這裡添加 return，以避免執行後續的程式碼
+            }
+            else
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // 打開連接
+                    connection.Open();
+
+                    string query = "SELECT * FROM member1 WHERE username = @username AND password = @password";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@username", username);
+                        command.Parameters.AddWithValue("@password", password);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // 檢查是否有符合的結果
+                            if (reader.HasRows)
+                            {
+                                // 有符合的結果，表示帳號密碼正確
+                                MessageBox.Show("登入成功");
+                                this.Hide();
+                                Form2 form2 = new Form2(username);
+                                form2.Show(); ;
+                            }
+                            else
+                            {
+                                // 沒有符合的結果，表示帳號密碼錯誤
+                                MessageBox.Show("錯誤：帳號或密碼錯誤");
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
---
+++ 登入/登入/Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace 登入
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string username)
+        {
+            InitializeComponent();
+            this.Text = "Form2";
+
+            string connectionString = "Data Source=DESKTOP-RDBCCLG\\SQLEXPRESS;Initial Catalog = membersystem;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // 打開連接
+                connection.Open();
+
+                string query = "SELECT * FROM member1 WHERE username = @username ";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // 檢查是否有符合的結果
+                        if (reader.HasRows)
+                        {
+                            // 逐筆讀取結果
+                            while (reader.Read())
+                            {
+                                // 使用索引或欄位名稱讀取資料
+                                string name = reader["name"].ToString();
+                                string phone = reader["phone"].ToString();
+                                string gender = reader["gender"].ToString();
+                                string address = reader["address"].ToString();
+                                string mail = reader["mail"].ToString();
+
+                                label2.Text = name;
+                                label4.Text = phone;
+                                label6.Text = gender;
+                                label8.Text = address;
+                                label10.Text = mail;
+                            }
+                        }
+                        else
+                        {
+                            // 沒有符合的結果
+                            MessageBox.Show("沒有結果");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
